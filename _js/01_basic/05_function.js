fdce23512196c771df6edd629beee39a6e60ba74
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-05-17 17:22:26 +0900

    learn js

---
+++ _js/01_basic/05_function.js
@@ -0,0 +1,8 @@
+console.log(`----------------------------------------`);
+console.log(`function.js에서 실행되는 코드`);
+console.log(`----------------------------------------`);
+
+function outerFunc() {
+    console.log(`funtion.js의 outerFunc에서 실행되는 코드`);
+    
+}
