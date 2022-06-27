ad56b73add85063d38638e21855b000245ddee3c
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-06-27 14:15:31 +0900

    Learn java

---
+++ _core/06_Package/src/built_in/access/AccessModifier.java
@@ -0,0 +1,17 @@
+package built_in.access;
+
+public class AccessModifier {
+
+	public static void main(String[] args) {
+		// 접근제한자
+		// public : 모든 외부 클래스에게 오픈
+		// private : 동일 클래스의 내부 멤버변수, 멤버메서드, 내부클래스
+		// 에게만 오픈
+		// 생략된(default) : 같은 패키지에 소속된 클래스에게만 오픈
+		// protected : extends, implements 가 적용된 클래스에게만 오픈
+
+		// public, private, default 는 범위접근제한
+		// protected는 범위제한자
+	}
+
+}
