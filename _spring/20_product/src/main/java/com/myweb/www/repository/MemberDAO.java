b0e6c5f972d986b67256959e4e2d4b0dcc3cd705
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-03 14:15:37 +0900

    Learn spring

---
+++ _spring/20_product/src/main/java/com/myweb/www/repository/MemberDAO.java
@@ -0,0 +1,16 @@
+package com.myweb.www.repository;
+
+import java.util.List;
+
+import com.myweb.www.domain.MemberVO;
+
+public interface MemberDAO {
+	int insert(MemberVO mvo); //register
+	MemberVO selectOne(MemberVO mvo); // login
+	List<MemberVO> selectList(); // list
+	MemberVO selectOne(String email); // detail
+	int update(MemberVO mvo); // modifiy
+	int delete(String email); // remove
+	int selectEmail(String email);
+	int update_lastLog(String email);
+}
---
+++ _spring/20_product/src/main/java/com/myweb/www/service/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package com.myweb.www.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.myweb.www.domain.ProductVO;
+import com.myweb.www.repository.ProductDAO;
+@Service
+public class ProductServiceImpl implements ProductService {
+	
+	@Inject
+	private ProductDAO pdao;
+	
+	@Override
+	public int register(ProductVO pvo) {
+		// TODO Auto-generated method stub
+		return pdao.insert(pvo);
+	}
+
+	@Override
+	public List<ProductVO> getList() {
+		// TODO Auto-generated method stub
+		return pdao.selectList();
+	}
+
+
+	@Override
+	public ProductVO getDetail(long pno) {
+		// TODO Auto-generated method stub
+		return pdao.selectOne(pno);
+	}
+
+	@Override
+	public int modifiy(ProductVO pvo) {
+		// TODO Auto-generated method stub
+		return pdao.update(pvo);
+	}
+
+	@Override
+	public int remove(long pno) {
+		// TODO Auto-generated method stub
+		return pdao.delete(pno);
+	}
+
+}
