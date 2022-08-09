5ce78c3ec1cee6eb31db86a651a9c491bd0d62b2
X2X2XE <58972100+X2X2XE@users.noreply.github.com>
2022-08-09 14:12:39 +0900

    Learn spring

---
+++ _spring/60_file_attach/src/main/java/com/myweb/www/domain/MemberDTO.java
@@ -0,0 +1,20 @@
+package com.myweb.www.domain;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class MemberDTO {
+	private MemberVO mvo;
+	
+	private FileVO file;
+	
+	
+}
